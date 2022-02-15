da63c41639f21058892b2792630e1fcf191c9df9
Veronica <vicareon@gmail.com>
2022-02-15 09:27:45 -0300

    Commit Inicial

---
+++ src/acelerazg/Card.java
@@ -0,0 +1,104 @@
+package acelerazg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Date;
+
+public class Card implements Comparable<Card>{
+    private String nome, descricao, categoria, status;
+    private int prioridade;
+    private Date dataTermino;
+
+    public Card(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public Date getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(Date dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setarStatus(BufferedReader leitura) throws IOException {
+        System.out.println("Digite a opção desejada de status: " +
+                "\n1. A Fazer" +
+                "\n2. Em Progresso" +
+                "\n3. Feito");
+        int a = Integer.parseInt(leitura.readLine());
+        if(a == 1){
+            this.setStatus("A Fazer");
+        }
+        else if(a == 2){
+            this.setStatus("Em Progresso");
+        }
+        else if(a == 3){
+            this.setStatus("Feito");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", status='" + status + '\'' +
+                ", prioridade=" + prioridade +
+                ", dataTermino=" + dataTermino +
+                '}';
+    }
+
+    public int compareTo(Card o) {
+        if(prioridade > o.prioridade){
+            return 1;
+        }
+        else if(prioridade < o.prioridade){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
---
+++ src/acelerazg/Main.java
@@ -0,0 +1,73 @@
+package acelerazg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ParseException {
+        ArrayList<Card> listaDeCards = new ArrayList<Card>();
+        BufferedReader leitura = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("*** Menu Inicial ***\n" +
+                "Atualmente, vc tem " + listaDeCards.size() + " cards na sua coleção." +
+                "\n1. Adicionar um novo card" +
+                "\n2. Mostrar cards por prioridade" +
+                "\n3. Excluir card" +
+                "\nDigite a opção desejada: ");
+        int escolhaMenu = Integer.parseInt(leitura.readLine());
+
+        switch (escolhaMenu){
+            case 1:
+                Card card = new Card();
+                cadastroCard(card, leitura);
+                listaDeCards.add(card);
+                rebalanceamento(listaDeCards);
+                System.out.println("Card adicionado com sucesso!");
+            case 2:
+                mostrarCards(listaDeCards);
+
+            case 3:
+                removerCard(listaDeCards, leitura);
+        }
+    }
+
+    public static void cadastroCard(Card card, BufferedReader leitura) throws IOException, ParseException {
+        System.out.print("*** Cadastro de Card ***\n" +
+                "Digite o nome do card: ");
+        card.setNome(leitura.readLine());
+        System.out.print("Digite a descrição: ");
+        card.setDescricao(leitura.readLine());
+        System.out.print("Digite a categoria: ");
+        card.setCategoria(leitura.readLine());
+        System.out.print("Digite a prioridade ( 1 a 5 ): ");
+        card.setPrioridade(Integer.parseInt(leitura.readLine()));
+        System.out.print("Digite a data de término: ");
+        String inputData = leitura.readLine();
+        Date dataTermino = new SimpleDateFormat("dd/MM/yyyy").parse(inputData);
+        card.setDataTermino(dataTermino);
+        card.setarStatus(leitura);
+    }
+
+    public static void mostrarCards(ArrayList listaDeCards){
+        for(Object c : listaDeCards){
+            System.out.println(c.toString());
+        }
+    }
+
+    public static void removerCard(ArrayList listaDeCards, BufferedReader leitura) throws IOException {
+        System.out.print("Digite o número do card que deseja remover: ");
+        int i = Integer.parseInt(leitura.readLine());
+        listaDeCards.remove(i - 1);
+        System.out.println("Card removido com sucesso!");
+    }
+
+    public static void rebalanceamento(ArrayList listaDeCards){
+        Collections.sort(listaDeCards);
+    }
+}
