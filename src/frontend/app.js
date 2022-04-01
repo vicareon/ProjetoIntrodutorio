1208c39765a61d30b1a09c351e285ea95824443e
Veronica <vicareon@gmail.com>
2022-03-31 21:05:09 -0300

    correções, melhorias e regex
    criação da classe card e implementação do cadastro de card, correção de atributos trocados, adição de validação com regex

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,33 +1,80 @@
-const botaoCriar = document.getElementById("botaoCriar");
-const botaoAtualizar = document.getElementById("botaoAtualizar");
-const botaoExcluir = document.getElementById("botaoExcluir");
-const botaoFecharJanelaModal = document.getElementById("botaoFecharJanelaModal");
+const botaoAbrirJanelaCadastro = document.getElementById("botaoCriar");
+const botaoAtualizarCard = document.getElementById("botaoAtualizar");
+const botaoExcluirCard = document.getElementById("botaoExcluir");
+const botaoFecharJanelaCadastro = document.getElementById("botaoFecharJanelaModal");
 const modalContainerCadastro = document.getElementById("modal_container_cadastro");
 
 let listaCards = [];
+const regexPrioridade = /[^1-5]/;
+let hoje = new Date();
 
-botaoCriar.addEventListener('click', () => {
+botaoAbrirJanelaCadastro.addEventListener('click', () => {
     modalContainerCadastro.classList.add('show');
 })
 
-botaoFecharJanelaModal.addEventListener('click', () => {
+botaoFecharJanelaCadastro.addEventListener('click', () => {
+    cadastroCard();
+    resetarFormulario();
     modalContainerCadastro.classList.remove('show');
 })
 
+/*botaoExcluirCard.addEventListener('click', () => {
+    //card.removerCard();
+})*/
+
+function resetarFormulario(){
+    document.getElementById("formularioCadastro").reset();
+}
+
 function cadastroCard(){
-    let card = {
-        nome: document.getElementById("nome").value,
-        categoria: document.getElementById("categoria").value,
-        status: document.getElementById("status"),
-        prioridade: document.getSelection(),
+    let nome = document.getElementById("nomeCard").value;
+    let descricao = document.getElementById("descricaoCard").value;
+    let categoria = document.getElementById("categoriaCard").value;
+    let prioridade = document.getElementById("prioridade").value;
+    if(regexPrioridade.test(prioridade)){
+        //corrigir depois - manter na tela de cadastro para não perder dados
+        alert("Prioridade inválida!");
     }
+    let status = document.getElementById("status").value;
+    if(status === "status"){
+        alert("Status inválido!");
+    }
+    let dataTermino = document.getElementById("dataTermino").value;
+
+    let validacaoDataTermino = dataTermino.split(/-/g);
+
+    if(hoje.getFullYear() < validacaoDataTermino[0]){
+        alert("Data inválida!");
+    }
+    else if(hoje.getMonth() < validacaoDataTermino[1]){
+        alert("Data inválida!");
+    }
+    else if(hoje.getDay() < validacaoDataTermino[2]){
+        alert("Data inválida!");
+    }
+
+    let card = new Card(nome, descricao, categoria, status, prioridade, dataTermino);
     listaCards.push(card);
+    console.log(card);
+    console.log(listaCards.length);
 }
 
-function removerCard(){
+class Card{
+    constructor(nome, descricao, categoria, status, prioridade, dataTermino) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.status = status;
+        this.prioridade = prioridade;
+        this.dataTermino = dataTermino;
+    }
 
-}
+    removerCard(card){
+        let i = listaCards.findIndex(card.nome);
+        listaCards.splice(i, 1);
+    }
 
-function atualizarCard(){
+    atualizarCard(){
 
+    }
 }
